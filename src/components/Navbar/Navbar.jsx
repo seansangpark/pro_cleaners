b3c40daa327af1e6e5b149fd1b7922a02aa9314f
Sean Park <seansangpark@gmail.com>
2023-08-29 09:43:29 -0400

    icon implemented

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import { MdOutlineClose } from 'react-icons/md';
 
 import images from '../../constants/images';
 import './Navbar.css';
@@ -57,7 +57,7 @@ const Navbar = () => {
 
         {toggleMenu && (
           <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
-            <MdOutlineRestaurantMenu
+            <MdOutlineClose
               fontSize={27}
               className='overlay__close'
               onClick={() => setToggleMenu(false)}

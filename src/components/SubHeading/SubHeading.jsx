23c7bb59bb48c2e7ace45b443fa187803c9ac355
Sean Park <seansangpark@gmail.com>
2023-08-29 13:16:27 -0400

    white line added

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 const SubHeading = ({ title }) => (
   <div style={{ marginBottom: '1rem' }}>
     <p className='p__cormorant'>{title}</p>
+    <div
+      class='white-line'
+      style={{ border: 'none', borderTop: '1px solid white', margin: '10px 0' }}
+    ></div>
   </div>
 );
 

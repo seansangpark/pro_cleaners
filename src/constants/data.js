c86e6abb59f32583685307a91a27fadd1d184117
Sean Park <seansangpark@gmail.com>
2023-08-31 14:56:36 -0400

    modifying dry cleaning data and applying menu items

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,34 +1,134 @@
 import images from './images';
 
-const wines = [
+const dry_cleanings = [
   {
-    title: 'Chapel Hill Shiraz',
-    price: '$56',
-    tags: 'AU | Bottle',
+    title: '3/4 Coat',
+    price: '$13.99',
+    tags: '',
   },
   {
-    title: 'Catena Malbee',
-    price: '$59',
-    tags: 'AU | Bottle',
+    title: 'Blanket',
+    price: '$17.99',
+    tags: '',
   },
   {
-    title: 'La Vieillw Rose',
-    price: '$44',
-    tags: 'FR | 750 ml',
+    title: 'Blazer',
+    price: '$6.99',
+    tags: '',
   },
   {
-    title: 'Rhino Pale Ale',
-    price: '$31',
-    tags: 'CA | 750 ml',
+    title: 'Blouse',
+    price: '$5.25',
+    tags: '',
   },
   {
-    title: 'Irish Guinness',
-    price: '$26',
-    tags: 'IE | 750 ml',
+    title: 'Comforter',
+    price: '$35.00',
+    tags: '',
+  },
+  {
+    title: 'Dress',
+    price: '$11.99',
+    tags: '',
+  },
+  {
+    title: 'Golf Shirt',
+    price: '$4.75',
+    tags: '',
+  },
+  {
+    title: 'Jacket',
+    price: '$6.99',
+    tags: '',
+  },
+  {
+    title: 'Jeans',
+    price: '$5.75',
+    tags: '',
+  },
+  {
+    title: 'Jersey',
+    price: '$4.99',
+    tags: '',
+  },
+  {
+    title: 'Pants',
+    price: '$5.23',
+    tags: '',
+  },
+  {
+    title: 'Robe',
+    price: '$12.99',
+    tags: '',
+  },
+  {
+    title: 'Shirt',
+    price: '$4.25',
+    tags: '',
+  },
+  {
+    title: 'Shorts',
+    price: '$4.75',
+    tags: '',
+  },
+  {
+    title: 'Skirt',
+    price: '$5.75',
+    tags: '',
+  },
+  {
+    title: 'Suit 2PC',
+    price: '$12.99',
+    tags: '',
+  },
+  {
+    title: 'Suit 3PC',
+    price: '$16.50',
+    tags: '',
+  },
+  {
+    title: 'Sweater',
+    price: '$6.00',
+    tags: '',
+  },
+  {
+    title: 'T-Shirt',
+    price: '$4.79',
+    tags: '',
+  },
+  {
+    title: 'Vest',
+    price: '$3.99',
+    tags: '',
   },
 ];
 
-const cocktails = [
+const repairs = [
+  {
+    title: 'Aperol Sprtiz',
+    price: '$20',
+    tags: 'Aperol | Villa Marchesi prosecco | soda | 30 ml',
+  },
+  {
+    title: "Dark 'N' Stormy",
+    price: '$16',
+    tags: 'Dark rum | Ginger beer | Slice of lime',
+  },
+  {
+    title: 'Daiquiri',
+    price: '$10',
+    tags: 'Rum | Citrus juice | Sugar',
+  },
+  {
+    title: 'Old Fashioned',
+    price: '$31',
+    tags: 'Bourbon | Brown sugar | Angostura Bitters',
+  },
+  {
+    title: 'Negroni',
+    price: '$26',
+    tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
+  },
   {
     title: 'Aperol Sprtiz',
     price: '$20',
@@ -79,4 +179,4 @@ const awards = [
   },
 ];
 
-export default { wines, cocktails, awards };
+export default { dry_cleanings, repairs, awards };
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,64 @@
 import React from 'react';
 
+import { SubHeading, MenuItem } from '../../components';
+import { images, data } from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div
+    className='app__specialMenu flex__center section__padding'
+    id='menu'
+  >
+    <div className='app__specialMenu-title'>
+      <SubHeading title='Menu that fits you palatte' />
+      <h1 className='headtext__cormorant'>Today's Special</h1>
+    </div>
+
+    <div className='app__specialMenu-menu'>
+      <div className='app__specialMenu-menu_wine flex__center'>
+        <p className='app__specialMenu-menu_heading'>Dry Cleaning</p>
+        <div className='app__specialMenu_menu_items'>
+          {data.dry_cleanings.map((item, index) => (
+            <MenuItem
+              key={item.title + index}
+              title={item.title}
+              price={item.price}
+              tags={item.tags}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className='app__specialMenu-menu_img'>
+        <img
+          src={images.menu}
+          alt='menu img'
+        />
+      </div>
+
+      <div className='app__specialMenu-menu_cocktails flex__center'>
+        <p className='app__specialMenu-menu_heading'>Repairs & Alterations</p>
+        <div className='app__specialMenu_menu_items'>
+          {data.repairs.map((item, index) => (
+            <MenuItem
+              key={item.title + index}
+              title={item.title}
+              price={item.price}
+              tags={item.tags}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+
+    <div style={{ marginTop: '15px' }}>
+      <button
+        type='button'
+        className='custom__button'
+      >
+        View More
+      </button>
+    </div>
   </div>
 );
 
